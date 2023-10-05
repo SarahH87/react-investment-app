6e1bb57e2d0ca44910fd360b7bb2ca2872158239
SarahH <sarah.heanan@komododigital.co.uk>
2023-10-05 16:13:05 +0100

    first unsuccessful attempt at using state, pass dummy data to calculate function

--- src/App.js
+++ src/App.js
@@ -3,44 +3,97 @@ import InvestmentForm from "./components/InvestmentForm";
 import InvestmentsList from "./components/InvestmentsList";
 
 function App() {
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+  // const calculateHandler = (userInput) => {
+  //   // Should be triggered when form is submitted
+  //   // You might not directly want to bind it to the submit event on the form though...
+  //
+  //   const yearlyData = []; // per-year results
+  //
+  //   let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+  //   const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+  //   const expectedReturn = +userInput['expected-return'] / 100;
+  //   const duration = +userInput['duration'];
+  //
+  //   // The below code calculates yearly results (total savings, interest etc)
+  //   for (let i = 0; i < duration; i++) {
+  //     const yearlyInterest = currentSavings * expectedReturn;
+  //     currentSavings += yearlyInterest + yearlyContribution;
+  //     yearlyData.push({
+  //       // feel free to change the shape of the data pushed to the array!
+  //       year: i + 1,
+  //       yearlyInterest: yearlyInterest,
+  //       savingsEndOfYear: currentSavings,
+  //       yearlyContribution: yearlyContribution,
+  //     });
+  //   }
+  //
+  //   // do something with yearlyData ...
+  // };
 
-    const yearlyData = []; // per-year results
+    const saveInvestmentData = (userInput) => {
+        console.log('Data saving');
 
-    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
+        const yearlyData = []; // per-year results
 
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
+        let currentSavings = +userInput.savings;
+        const yearlyContribution = +userInput.yearlySavings;
+        const expectedReturn = +userInput.expectedInterest / 100;
+        const duration = +userInput.investmentDuration;
 
-    // do something with yearlyData ...
-  };
+        // The below code calculates yearly results (total savings, interest etc)
+        for (let i = 0; i < duration; i++) {
+            const yearlyInterest = currentSavings * expectedReturn;
+            currentSavings += yearlyInterest + yearlyContribution;
+            yearlyData.push({
+                // feel free to change the shape of the data pushed to the array!
+                year: i + 1,
+                yearlyInterest: yearlyInterest,
+                savingsEndOfYear: currentSavings,
+                yearlyContribution: yearlyContribution,
+            });
+        }
 
-  return (
-    <div>
-      <Header />
+        console.log(yearlyData);
+    };
 
-      <InvestmentForm />
+    const calculateHandler = (userInput) => {
+        // Should be triggered when form is submitted
+        // You might not directly want to bind it to the submit event on the form though...
 
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-      <InvestmentsList />
-    </div>
-  );
+        const yearlyData = []; // per-year results
+
+        let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+        const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+        const expectedReturn = +userInput['expected-return'] / 100;
+        const duration = +userInput['duration'];
+
+        // The below code calculates yearly results (total savings, interest etc)
+        for (let i = 0; i < duration; i++) {
+            const yearlyInterest = currentSavings * expectedReturn;
+            currentSavings += yearlyInterest + yearlyContribution;
+            yearlyData.push({
+                // feel free to change the shape of the data pushed to the array!
+                year: i + 1,
+                yearlyInterest: yearlyInterest,
+                savingsEndOfYear: currentSavings,
+                yearlyContribution: yearlyContribution,
+            });
+        }
+
+        // do something with yearlyData ...
+    };
+
+    return (
+        <div>
+          <Header />
+
+          <InvestmentForm onSaveInvestmentData={saveInvestmentData} />
+
+          {/* Todo: Show below table conditionally (only once result data is available) */}
+          {/* Show fallback text if no data is available */}
+          <InvestmentsList yearlyData={yearlyData} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/InvestmentForm.js
+++ src/components/InvestmentForm.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const InvestmentForm = () => {
+const InvestmentForm = (props) => {
+    const [enteredSavings, setEnteredSavings] = useState('');
+    const [yearlySavings, setYearlySavings] = useState('');
+    const [expectedInterest, setExpectedInterest] = useState('');
+    const [investmentDuration, setInvestmentDuration] = useState('');
+
+    const savingsChangeHandler = (event) => {
+        setEnteredSavings(event.target.value);
+        console.log('called');
+    };
+
+    const yearlySavingsChangeHandler = (event) => {
+        setYearlySavings(event.target.value);
+    };
+
+    const expectedInterestHandler = (event) => {
+        setExpectedInterest(event.target.value);
+    };
+
+    const investmentDurationHandler = (event) => {
+        setInvestmentDuration(event.target.value);
+    };
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
         console.log('submitted');
+
+        // const investmentData = {
+        //     savings: enteredSavings,
+        //     yearlySavings: yearlySavings,
+        //     expectedInterest: expectedInterest,
+        //     investmentDuration: investmentDuration,
+        // }
+
+        const investmentData = {
+            savings: 100,
+            yearlySavings: 1000,
+            expectedInterest: 3,
+            investmentDuration: 3,
+        }
+
+        // console.log(investmentData);
+        props.onSaveInvestmentData(investmentData);
+
+        setEnteredSavings('');
+        setYearlySavings( '');
+        setExpectedInterest('');
+        setInvestmentDuration('');
     };
 
     const resetButtonHandler = (event) => {
@@ -16,11 +59,19 @@ const InvestmentForm = () => {
             <div className="input-group">
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings" />
+                    <input type="number"
+                           id="current-savings"
+                           // value={enteredSavings}
+                           onChange={setEnteredSavings}
+                    />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input type="number" id="yearly-contribution" />
+                    <input type="number"
+                           id="yearly-contribution"
+                           // value={yearlySavings}
+                           onChange={setYearlySavings}
+                    />
                 </p>
             </div>
             <div className="input-group">
@@ -28,11 +79,19 @@ const InvestmentForm = () => {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input type="number" id="expected-return" />
+                    <input type="number"
+                           id="expected-return"
+                           // value={expectedInterest}
+                           onChange={setExpectedInterest}
+                    />
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
-                    <input type="number" id="duration" />
+                    <input type="number"
+                           id="duration"
+                           // value={expectedInterest}
+                           onChange={setInvestmentDuration}
+                    />
                 </p>
             </div>
             <p className="actions">
